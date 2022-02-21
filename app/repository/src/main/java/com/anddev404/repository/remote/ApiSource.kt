b5cf9bf69ed8c5b9a3e36503b3beb149d541b735
anddev404 <marcinek07@gmail.com>
2022-02-21 21:38:24 +0100

    Create ApiSource.kt

---
+++ app/repository/src/main/java/com/anddev404/repository/remote/ApiSource.kt
@@ -0,0 +1,8 @@
+package com.anddev404.repository.remote
+
+enum class ApiSource(val sourceApi: String) {
+    FAKE_API("Fake"),
+    TECH_NEWS("Tech news"),
+    NEWS("News"),
+    TECH_INFO("Tech Info")
+}
