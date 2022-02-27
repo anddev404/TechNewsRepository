aedb8655f53b80995ac65ed58846f88ff5115772
anddev404 <marcinek07@gmail.com>
2022-02-27 12:31:43 +0100

    refactor move Fake_api class

--- app/repository/src/main/java/com/anddev404/repository/Repository.kt
+++ app/repository/src/main/java/com/anddev404/repository/Repository.kt
@@ -1,7 +1,7 @@
 package com.anddev404.repository
 
 import com.anddev404.repository.remote.ApiSource
-import com.anddev404.repository.remote.FakeApi
+import com.anddev404.repository.remote.fake_Api.FakeApi
 import com.anddev404.repository.remote.ApiInterface
 
 class Repository {
--- app/repository/src/main/java/com/anddev404/repository/remote/fake_Api/FakeApi.kt
+++ app/repository/src/main/java/com/anddev404/repository/remote/fake_Api/FakeApi.kt
@@ -1,12 +1,13 @@
-package com.anddev404.repository.remote
+package com.anddev404.repository.remote.fake_Api
 
 import com.anddev404.repository.model.News
 import com.anddev404.repository.model.SingularNews
+import com.anddev404.repository.remote.ApiInterface
 import kotlinx.coroutines.delay
 
 class FakeApi : ApiInterface {
 
-    override suspend fun getNews(): News {
+    override suspend fun getNews(source: String): News {
         delay(1000)
         return News(
             arrayListOf(
