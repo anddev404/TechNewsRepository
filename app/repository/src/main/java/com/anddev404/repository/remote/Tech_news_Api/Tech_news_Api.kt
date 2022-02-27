df335b96fe69f82b13948bf9e544b8f62b074ae9
anddev404 <marcinek07@gmail.com>
2022-02-27 18:34:55 +0100

    create 'Tech news' class and implement ApiInterface

---
+++ app/repository/src/main/java/com/anddev404/repository/remote/Tech_news_Api/Tech_news_Api.kt
@@ -0,0 +1,46 @@
+package com.anddev404.repository.remote.Tech_news_Api
+
+import android.util.Log
+import com.anddev404.repository.model.News
+import com.anddev404.repository.model.SingularNews
+import com.anddev404.repository.remote.ApiInterface
+
+class Tech_news_Api : ApiInterface {
+
+    var api: Tech_news_Retrofit_Service
+
+    init {
+        api = Tech_news_Retrofit_Service.Tech_news_ApiService
+    }
+
+    override suspend fun getNewsOrEmptyList(source: String): News {
+
+        var newsApi = api.getNews("gsmarena")///TODO temporary source is 'gsmarena'
+
+        if (newsApi.code() == 200) {
+
+            var newsList = newsApi.body() ?: arrayListOf()
+
+            return convertTechNewsResponseToNewsModel(newsList)
+        }
+        return News(arrayListOf())
+    }
+
+    private fun convertTechNewsResponseToNewsModel(responseTechNews: List<Response_Tech_news>): News {
+
+        var news = News()
+
+        for (singularNews in responseTechNews) {
+            (news.news as ArrayList).add(
+                SingularNews(
+                    singularNews.title,
+                    singularNews.link,
+                    singularNews.img,
+                    singularNews.dateTime
+                )
+            )
+        }
+
+        return news
+    }
+}
