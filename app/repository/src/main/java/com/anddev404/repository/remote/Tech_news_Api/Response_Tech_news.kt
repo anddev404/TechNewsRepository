61e0c8ae52be367561c5436c1069863db0b5de19
anddev404 <marcinek07@gmail.com>
2022-02-27 12:41:53 +0100

    Create Response for "Tech news" api

---
+++ app/repository/src/main/java/com/anddev404/repository/remote/Tech_news_Api/Response_Tech_news.kt
@@ -0,0 +1,11 @@
+package com.anddev404.repository.remote.Tech_news_Api
+
+
+data class Response_Tech_news(
+    var title: String = "",
+    val para: String = "",
+    val img: String = "",
+    val dateTime: String = "",
+    val link: String = ""
+) {
+}
