aa02c341e6a7fbdfd3639a19c35a0a0d4dfffa2b
anddev404 <marcinek07@gmail.com>
2022-02-27 12:42:32 +0100

    add api key to "Tech news" api

--- app/repository/src/main/java/com/anddev404/repository/remote/Tech_news_Api/Response_Tech_news.kt
+++ app/repository/src/main/java/com/anddev404/repository/remote/Tech_news_Api/Response_Tech_news.kt
@@ -8,4 +8,9 @@ data class Response_Tech_news(
     val dateTime: String = "",
     val link: String = ""
 ) {
+
+    companion object {
+        const val apiKey = "a8c8f09426mshbe1aa1787990dd7p12d76ejsn484423dcef66"
+        const val xRapidapiHost = "tech-news3.p.rapidapi.com"
+    }
 }
