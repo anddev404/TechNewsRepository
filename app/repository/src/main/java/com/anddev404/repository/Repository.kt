824396ad1db553cf3ce383797a1bff2f90b701b3
anddev404 <marcinek07@gmail.com>
2022-02-27 18:36:50 +0100

    add "Tech news" api to Repository

--- app/repository/src/main/java/com/anddev404/repository/Repository.kt
+++ app/repository/src/main/java/com/anddev404/repository/Repository.kt
@@ -1,15 +1,19 @@
 package com.anddev404.repository
 
+import com.anddev404.repository.model.News
 import com.anddev404.repository.remote.ApiSource
 import com.anddev404.repository.remote.fake_Api.FakeApi
 import com.anddev404.repository.remote.ApiInterface
+import com.anddev404.repository.remote.Tech_news_Api.Tech_news_Api
+import com.anddev404.repository.remote.Tech_news_Api.Tech_news_Retrofit_Service
 
 class Repository {
 
     fun getApi(source: ApiSource): ApiInterface {
 
         when (source) {
-            ApiSource.FAKE_API -> FakeApi()
+            ApiSource.FAKE_API -> return FakeApi()
+            ApiSource.TECH_NEWS -> return Tech_news_Api()
 
         }
         return FakeApi()//todo change 'return' to exception or default api
