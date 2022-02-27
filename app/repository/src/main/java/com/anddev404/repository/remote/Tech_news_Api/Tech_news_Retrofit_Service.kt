cb417f6dc4f1fdba74a7f044349be19af8f9b3af
anddev404 <marcinek07@gmail.com>
2022-02-27 18:15:06 +0100

    add retrofit's builder for "Tech news" api

--- app/repository/src/main/java/com/anddev404/repository/remote/Tech_news_Api/Tech_news_Retrofit_Service.kt
+++ app/repository/src/main/java/com/anddev404/repository/remote/Tech_news_Api/Tech_news_Retrofit_Service.kt
@@ -1,11 +1,26 @@
 package com.anddev404.repository.remote.Tech_news_Api
 
+import okhttp3.OkHttpClient
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
 
 interface Tech_news_Retrofit_Service {
+
+    companion object {
+
+        private val retrofit = Retrofit.Builder()
+            .baseUrl(Response_Tech_news.baseUrl)
+            .client(OkHttpClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val Tech_news_ApiService = retrofit.create(Tech_news_Retrofit_Service::class.java)
+
+    }
 
     @Headers(
         "${Response_Tech_news.apiKeyName}: ${Response_Tech_news.apiKeyValue}",
