bac57815b2fa0709d530094684717b3a0b684f35
anddev404 <marcinek07@gmail.com>
2023-01-18 20:49:52 +0100

    add new source of news

--- app/repository/src/main/java/com/anddev404/repository/remote/ApiSource.kt
+++ app/repository/src/main/java/com/anddev404/repository/remote/ApiSource.kt
@@ -3,6 +3,7 @@ package com.anddev404.repository.remote
 enum class ApiSource(val sourceApi: String) {
     FAKE_API("Fake"),
     TECH_NEWS("Tech news"),
+    BING_NEWS_SEARCH("Bing News Search"),
     NEWS("News"),
     TECH_INFO("Tech Info")
 }
